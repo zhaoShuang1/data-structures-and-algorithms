9f974e276acb0764b1ccc1c5326a80782c4a6708
zhaoshuang1 <18310475049@163.com>
2019-04-08 18:34:15 +0800

    add selectsort

--- src/sort/BubbleSortTest.java
+++ src/sort/BubbleSortTest.java
@@ -1,7 +1,15 @@
 package sort;
 
 import java.util.Arrays;
-
+/**
+ * 比较相邻的元素。如果第一个比第二个大，就交换他们两个。
+ * 对每一对相邻元素做同样的工作，从开始第一对到结尾的最后一对。在这一点，最后的元素应该会是最大的数。
+ * 针对所有的元素重复以上的步骤，除了最后一个。
+ * 持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。	
+ * 
+ * 冒泡排序最好的时间复杂度为 O(n)，最坏时间复杂度为O(n^2)
+ * 
+ */
 public class BubbleSortTest {
 	public static void main(String[] args) {
 		int[] arr = new int[] {21,34,5,7,1,4,8,12,3};
@@ -11,8 +19,9 @@ public class BubbleSortTest {
 	}
 
 	public static void bubbleSort(int[] arr) {
-		for(int i = 0 ; i < arr.length ; i ++) {
-			for(int j = i ; j < arr.length ; j ++ ) {
+		for(int i = 0 ; i < arr.length-1 ; i ++) {
+			for(int j = 0 ; j < arr.length-1-i ; j ++ ) {
+				//比较相邻的两个元素，如果当前元素大于下一个元素则交换位置
 				if(arr[j] > arr[j+1]) {
 					int temp = arr[j];
 					arr[j] = arr[j+1];
@@ -22,6 +31,4 @@ public class BubbleSortTest {
 		}
 		
 	}
-	
-	
 }
---
+++ src/sort/SelectSort.java
@@ -0,0 +1,40 @@
+package sort;
+
+import java.util.Arrays;
+/**
+ * 选择排序
+ * 选择排序是给每个位置选择当前元素最小的，比如给第一个位置选择最小的，在剩余元素里面给第二个元素选择第二小的，
+ * 依次类推，直到第n-1个元素，第n个元素不用选择了，因为只剩下它一个最大的元素了。
+ * 那么，在一趟选择，如果一个元素比当前元素小，而该小的元素又出现在一个和当前元素相等的元素后面，那么交换后稳定性就被破坏了。
+ * 比较拗口，举个例子，序列5 8 5 2 9，我们知道第一遍选择第1个元素5会和2交换，那么原序列中两个5的相对前后顺序就被破坏了，
+ * 所以选择排序是一个不稳定的排序算法。
+ * 
+ * 
+ * 
+ */
+public class SelectSort {
+	public static void main(String[] args) {
+		int[] arr = new int[] {21,34,5,7,1,4,8,12,3};
+		System.out.println(Arrays.toString(arr));
+		selectSort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+
+	public static void selectSort(int[] arr) {
+		int minIndex = 0;
+		int temp = 0;
+		for(int i = 0 ; i < arr.length ; i ++ ) {
+			minIndex = i;
+			for(int j = i+1 ; j < arr.length ; j ++) {
+				if(arr[minIndex] > arr[j]) {
+					minIndex = j;
+				}
+			}
+			if(minIndex!=i) {
+				temp = arr[i];
+				arr[i] = arr[minIndex];
+				arr[minIndex]=temp;
+			}
+		}
+	}
+}
